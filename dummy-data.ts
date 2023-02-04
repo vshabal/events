bd6771be44663deb42411a93cc27c1c7c750e885
Denys Leonchev <dleo@softserveinc.com>
2023-02-04 22:35:56 +0200

    Update dummy data and its filtering logic

--- dummy-data.ts
+++ dummy-data.ts
@@ -3,18 +3,18 @@ const DUMMY_EVENTS = [
     id: 'e1',
     title: 'Programming for everyone',
     location: 'Somerstreeet 25, 12345 San Somewhere',
-    date: '2021-04-12',
+    date: '2022-04-12',
     image: '/images/coding-event.jpg',
-    isFeatured: false,
+    isFeatured: false
   },
   {
     id: 'e2',
     title: 'Programming for somebody',
     location: 'Somerstreeet 52, 54321 San Somewhere',
-    date: '2021-04-12',
+    date: '2021-11-11',
     image: '/images/coding-event-1.jpg',
-    isFeatured: true,
-  },
+    isFeatured: true
+  }
 ];
 
 export function getFeaturedEvents() {
@@ -26,16 +26,22 @@ export function getAllEvents() {
 }
 
 type DateFilter = {
-  year: number;
-  month: number;
+  year?: number;
+  month?: number;
 };
 export function getFilteredEvents({ year, month }: DateFilter) {
   return DUMMY_EVENTS.filter((event) => {
     const eventDate = new Date(event.date);
     const eventDateFullYear = eventDate.getFullYear();
-    const doesEventYearMatchesFilter = eventDateFullYear === year;
+    const isEventYearFilteringNeeded = year !== undefined;
+    const doesEventYearMatchesFilter = isEventYearFilteringNeeded
+      ? eventDateFullYear === year
+      : true;
     const eventDateMonth = eventDate.getMonth();
-    const doesEventMonthMatchesFilter = eventDateMonth === month - 1;
+    const isEventMonthFilteringNeeded = month !== undefined;
+    const doesEventMonthMatchesFilter = isEventMonthFilteringNeeded
+      ? eventDateMonth === month - 1
+      : true;
 
     return doesEventYearMatchesFilter && doesEventMonthMatchesFilter;
   });
